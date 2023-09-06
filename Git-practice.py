6d0f298dbc1d420c72003b9dadf898766de24529
maxine-scott2025 Scott <flojo734@gmail.com>
2023-09-06 21:10:49 +0000

    task 1 done

--- Git-practice.py
+++ Git-practice.py
@@ -1,3 +1,11 @@
-first = 'Max'
-last = 'scott'
-print("My name is", first, last)
+toDoList = ["Math Homework", "Cook Dinner", "Fold Laundry"]
+
+def addItem(item):
+   # Finish this code
+   return toDoList
+
+userAns = input("Do you want to add to your list or quit? A/Q")
+while userAns == "A":
+   item = input("What item do you want to add?")
+   addItem(item)
+   userAns = input("Do you want to add to your list or quit? A/Q")
